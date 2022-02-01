4f3c8630d20420f3c5dd7d0b9824afb8fd8de52f
JinZr <zengrui.jin@outlook.com>
2022-02-01 23:07:17 +0800

    updated

---
+++ for_comparison/2021/Enhanced-Spatio-Temporal-Interaction-Learning-for-Video-Deraining/gen.sh
@@ -0,0 +1,1 @@
+python gen_json_for_model_testing.py --frame_dir=/home/desc/projects/derain/cu_rain_video_dataset/final-static-vid_frame --gt_dir=/home/desc/projects/derain/cu_rain_video_dataset/final-static-img --output_json=/home/desc/projects/derain/2021/Enhanced-Spatio-Temporal-Interaction-Learning-for-Video-Deraining/code/data/my_dataset.json
