ce1fe1aa0a8feb00bb89e0d478528d1195c36468
jinzr <60612200+JinZr@users.noreply.github.com>
2022-02-02 12:11:43 +0800

    Create gen.sh

---
+++ for_comparison/2021/Recurrent-Multi-Frame-Deraining/gen.sh
@@ -0,0 +1,3 @@
+python gen_list_for_model_testing.py \
+    --frame_dir=/home/desc/projects/derain/cu_rain_video_dataset/final-static-vid_frame --gt_dir=/home/desc/projects/derain/cu_rain_video_dataset/final-static-img \
+    --output_list=/home/desc/projects/derain/2021/Recurrent-Multi-Frame-Deraining/Recurrent_multi_frame_deraining_w_gan/lists/my_dataset.txt
