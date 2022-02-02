ca3765d5a62255789f56822f89e45aef2f62913d
jinzr <60612200+JinZr@users.noreply.github.com>
2022-02-02 12:09:07 +0800

    Create gen_list_for_model_testing.py

---
+++ for_comparison/2021/Recurrent-Multi-Frame-Deraining/gen_list_for_model_testing.py
@@ -0,0 +1,22 @@
+# generate a list of folders, each folder is associated with a video
+# and contains frames extracted from the video
+
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--frame_dir', type=str, required=True)
+parser.add_argument('--output_list', type=str, required=True)
+args = parser.parse_args()
+
+frame_dir = args.frame_dir
+output_list = args.output_list
+
+if __name__ == '__main__':
+    dirlist = os.listdir(frame_dir)
+    with open(output_list, 'w+') as fout:
+        fout.writelines(list(map(
+            lambda x: x + '\n',
+            dirlist
+        )))
