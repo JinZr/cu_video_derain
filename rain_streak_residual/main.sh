df0988dfa7524c21496acb5d1700280ded67c503
JinZr <zengrui.jin@outlook.com>
2022-03-23 13:34:16 +0800

    updated a script

---
+++ rain_streak_residual/main.sh
@@ -0,0 +1,4 @@
+python main.py \
+    --frames_pth=/home/desc/projects/derain/cu_rain_video_dataset/train/final-static-vid_frame/ \
+    --static_pth=/home/desc/projects/derain/cu_rain_video_dataset/train/final-static-img/ \
+    --overlay_pth=/home/desc/projects/derain/cu_rain_video_dataset/train/rain_streak/
