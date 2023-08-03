01098d9e10aa4abf5c1711421685505313de54db
JinZr <zengrui.jin@outlook.com>
2023-08-04 02:29:33 +0800

    updates

--- eval_niqe_fid.py
+++ eval_niqe_fid.py
@@ -3,12 +3,14 @@ import argparse
 import pyiqa
 import torch
 
+path = ""
+
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 fid_metric = pyiqa.create_metric("fid")
-score = fid_metric("")
+score = fid_metric(path)
 print("fid", score)
 
 
 niqe_metric = pyiqa.create_metric("niqe")
-score = niqe_metric("")
+score = niqe_metric(path)
 print("niqe", score)
