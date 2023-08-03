7d389caf3cd576d49b1177efa3752eb897783baf
zr_jin <60612200+JinZr@users.noreply.github.com>
2023-08-04 04:55:45 +0800

    updated

--- eval_niqe_fid.py
+++ eval_niqe_fid.py
@@ -1,16 +1,21 @@
-import argparse
+import glob
+import os
 
 import pyiqa
 import torch
 
-path = ""
+path = "/Users/zengruijin/Downloads/Ours/mid_motion"
+imgs = glob.glob(f"{path}/*/*.png", recursive=True, root_dir=path)
+dirs = os.listdir(path)
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-fid_metric = pyiqa.create_metric("fid")
-score = fid_metric(path)
-print("fid", score)
+if __name__ == "__main__":
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    fid_metric = pyiqa.create_metric("fid")
+    scores = 0
+    for dir in dirs:
+        scores += fid_metric(f"{path}/{dir}", dataset_split="trainval")
+    print("fid", scores)
 
-
-niqe_metric = pyiqa.create_metric("niqe")
-score = niqe_metric(path)
-print("niqe", score)
+    niqe_metric = pyiqa.create_metric("niqe")
+    score = niqe_metric(path)
+    print("niqe", score)
